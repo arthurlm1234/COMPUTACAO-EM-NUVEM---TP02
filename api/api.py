34b46a00d2b0e3ae46ea31752e45ddbb9e2432cb
Victor <victor.henrique5800@gmail.com>
2023-10-25 15:38:46 -0300

    Modelo de ML mudado

--- api/api.py
+++ api/api.py
@@ -1,26 +1,70 @@
 from flask import Flask, request, jsonify
 import pandas as pd
+import sys
+import pickle
 
 app = Flask(__name__)
 rules = pd.read_pickle('./../models/rules.pkl')
 
-@app.route('/api/recommend', methods=['POST'])
-def recommend():
-    songs = request.json['songs']
+def recommend(songs):
+    # Load song-artist mapping
+    song_artist_map = dict()
+    with open("./../models/songs_artists.pkl", "rb") as f:
+        song_artist_map = pickle.load(f)
+
     recommendations = []
     
+    if debug_mode:
+        print("DEBUG => RULES:\n\n", rules)
+        print()
+
     for song in songs:
-        matched_rules = rules[rules['antecedents'].apply(lambda x: song in x)]
-        if not matched_rules.empty:
-            recommendations.extend(matched_rules['consequents'].iloc[0])
+        # Get the artist of the song
+        artist = song_artist_map.get(song, None)
+        if artist is not None:
+            # Find rules where this artist is in the antecedents
+            matched_rules = rules[rules['antecedents'].apply(lambda x: artist in x)]
+            if not matched_rules.empty:
+                recommendations.extend(matched_rules['consequents'].iloc[0])
     
     recommendations = list(set(recommendations))
+    if len(recommendations) == 0: # The model can not recommend anything, so apply a random recommendation
+        index = hash("".join(songs)) % len(rules)
+        random_recommendation = rules["consequents"].iloc[index]
+        recommendations.extend(random_recommendation)
+
+    return recommendations
+
+@app.route('/api/recommend', methods=['POST'])
+def recommendApi():
+    songs = request.json['songs']
     
+    recommendations = recommend(songs)
     return jsonify({
         "playlist_ids": recommendations,
         "version": "1.0",
         "model_date": "2023-10-25"
     })
 
+def debugRecommendation():
+    # songs = ["I Was Married", "Hop A Plane", "Otra Vez (feat. J Balvin)", "Pierdo la Cabeza - Official Remix", "Ay Mi Dios"]
+    songs = ["Back In Your Head"]
+    # songs = ["HUMBLE."]
+    # songs = ["Force of Nature"]
+
+
+    recommendations = recommend(songs)
+
+    print("DEBUG => RECOMMENDATIONS:\n", recommendations)
+    print()
+
+debug_mode = False
 if __name__ == '__main__':
-    app.run(port=30500)
+    songs_artists = pd.read_pickle('./../models/songs_artists.pkl')
+    
+    if "--debug" in sys.argv:
+        debug_mode = True
+        debugRecommendation()
+
+    else:
+        app.run(port=30500)
--- ml/ml_trainer.py
+++ ml/ml_trainer.py
@@ -1,22 +1,54 @@
+import pickle
+import sys
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 
-def train_model(file_name):
+def preprocess(file_name):
     data = pd.read_csv(f"datasets/{file_name}")
-    transactions = data.groupby('pid')['track_name'].apply(list)
+    data = data.dropna()
+    data = data.drop_duplicates()
+    data = data.reset_index(drop=True)
 
-    te = TransactionEncoder()
-    te_ary = te.fit(transactions).transform(transactions)
+    return data
 
-    df = pd.DataFrame(te_ary, columns=te.columns_)
+def train_model(file_name):
+    data = preprocess(file_name)
+    songs_artists = dict(zip(data['track_name'], data['artist_name']))
+    
+    artist_most_freq_pid = data.groupby('artist_name')['pid'].apply(lambda x: x.value_counts().idxmax())
+    df_onehot = data.groupby(['pid','artist_name'])['artist_name'].count().unstack().fillna(0)
 
-    frequent_itemsets = apriori(df, min_support=0.01, use_colnames=True)
-    rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
+    def encode_units(x):
+        if x <= 0:
+            return False
+        if x >= 1:
+            return True
 
-    rules.to_pickle(f"../models/rules.pkl")
+    df_onehot = df_onehot.applymap(encode_units)
+    frequent_itemsets = apriori(df_onehot, min_support=0.008, use_colnames=True, verbose=1)
+    rules = association_rules(frequent_itemsets, metric="lift", min_threshold=0.001)
+
+    # Replace artist names in consequents with most frequent pid
+    rules['consequents'] = rules['consequents'].apply(lambda x: set(artist_most_freq_pid[i] for i in x))
+
+    if debug_mode:
+        # print("DEBUG => TRANSACTIONS:\n\n", transactions)
+        # print()
+        print("DEBUG => FREQUENT ITEMSETS:\n\n", frequent_itemsets)
+        print()
+        print("DEBUG => POSSIBLE RULES:\n\n", rules)
+
+    else:
+        rules.to_pickle(f"../models/rules.pkl")
+        with open("../models/songs_artists.pkl", "wb") as f:
+            pickle.dump(songs_artists, f)
 
 
+debug_mode = False
 if __name__ == "__main__":
+    if "--debug" in sys.argv:
+        debug_mode = True
+
     train_model("playlist-sample-ds1.csv")
