b994210ce1a0483a32b292835f2292ad5e03c7b1
Arthur <arthurlm1234@gmail.com>
2023-10-25 09:46:20 -0300

    rota criada

--- api/api.py
+++ api/api.py
@@ -1,18 +1,26 @@
+from flask import Flask, request, jsonify
 import pandas as pd
-from mlxtend.preprocessing import TransactionEncoder
-from mlxtend.frequent_patterns import apriori
-from mlxtend.frequent_patterns import association_rules
 
-def recommend_playlists(liked_songs):
-    rules = pd.read_pickle(f"../models/rules.pkl")
+app = Flask(__name__)
+rules = pd.read_pickle('./../models/rules.pkl')
 
-    # Filtra as regras que tem pelo menos um item que o usuário gosta
-    filtered_rules = rules[rules['antecedents'].apply(lambda x: any(item for item in liked_songs if item in x))]
-    recommended_songs = filtered_rules['consequents']
+@app.route('/api/recommend', methods=['POST'])
+def recommend():
+    songs = request.json['songs']
+    recommendations = []
+    
+    for song in songs:
+        matched_rules = rules[rules['antecedents'].apply(lambda x: song in x)]
+        if not matched_rules.empty:
+            recommendations.extend(matched_rules['consequents'].iloc[0])
+    
+    recommendations = list(set(recommendations))
+    
+    return jsonify({
+        "playlist_ids": recommendations,
+        "version": "1.0",
+        "model_date": "2023-10-25"
+    })
 
-    return recommended_songs
-
-
-liked_songs = ['HUMBLE.', 'Mask Off']
-recommendations = recommend_playlists(liked_songs)
-print(recommendations)
+if __name__ == '__main__':
+    app.run(port=30500)
