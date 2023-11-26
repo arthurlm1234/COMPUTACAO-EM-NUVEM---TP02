8af046d8dac88d97b56be05335360e56ca2691ee
arthurlm1234 <arthurlm1234@gmail.com>
2023-11-26 14:36:58 +0000

    commit

--- test_api.sh
+++ test_api.sh
@@ -1,5 +1,5 @@
 wget --server-response \
      --output-document response.out \
      --header='Content-Type: application/json' \
-     --post-data '{"songs": ["Yesterday", "Bohemian Rhapsody"]}' \
+     --post-data '{"songs": ["Yesterday", "Bohemian Rhapsody", "Hey Ya!"]}' \
      http://10.110.77.0:32171/api/recommend
